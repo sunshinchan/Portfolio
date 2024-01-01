c3568d17f5bf1d8559bbfe930efc5dc0257ae7c0
sunshinchan <sunilsolairaj@gmail.com>
2024-01-01 22:15:19 +0530

    portfolio
    html css js

---
+++ script.js
@@ -0,0 +1,9 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky",this.window.scrollY > 0);
+})
+let navlist = document.querySelector('.navlist');
+
+
+
